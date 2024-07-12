874696467e8b2e418a61b21772d83276f42ccf21
Victor Zornek <vzornek@gmail.com>
2024-07-12 16:37:25 -0300

    inicio da logica dos botoes do timer (add/sub/play/pause/reset)

--- src/FocusTimer/actions.js
+++ src/FocusTimer/actions.js
@@ -1,17 +1,32 @@
+import * as elements from "./elements.js"
+import state from "./state.js"
+
 export function toggleRunning() {
     console.log('estamos no togglerunning')
     document.documentElement.classList.toggle('running')
 }
 
 export function reset() {
-    console.log('reset aplicação')
+    state.minutes = 10
     document.documentElement.classList.remove('running')
+    
+    console.log(state.minutes)
 }
 
-export function addMinutes() {
-    console.log('adiciona 5 minutos no timer')
+export function addMinutes() { // add 5 min no timer
+    if(state.minutes > 90) {
+        return
+    }
+    
+    state.minutes += 5
+    console.log(state.minutes)
 }
 
-export function subMinutes() {
-    console.log('subtrai 5 minutos no timer')
+export function subMinutes() {  // sub 5 min no timer
+    if(state.minutes < 0) {
+        return
+    }
+
+    state.minutes -= 5
+    console.log(state.minutes)
 }
